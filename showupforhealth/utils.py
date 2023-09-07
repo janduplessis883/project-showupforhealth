c443c02c20066fd67648bfd120e6e5f7ddfdab0f
Alessio Robotti <alessiorobotti1@gmail.com>
2023-09-07 18:51:56 +0100

    Added StandardScaler function

---
+++ showupforhealth/utils.py
@@ -0,0 +1,8 @@
+from sklearn.preprocessing import StandardScaler
+
+def standard_scaler(X_train, X_test):
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_test_scaled

9b143324d1fb680f98d57c78f2eefc0cf7b4e4ea
michael melis <michaelmelisjhb@gmail.com>
2023-09-07 20:35:35 +0100

    issue 27 evaluate model

--- showupforhealth/utils.py
+++ showupforhealth/utils.py
@@ -1,3 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
+from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, roc_curve, auc
 from sklearn.model_selection import train_test_split
 
 # X = df.drop('Appointment_status', axis=1)
@@ -16,5 +20,63 @@ def oversample_with_smote(X_train, y_train, sampling_strategy='auto', k_neighbor
     print('Shape of X_train:', np.shape(X_train_oversampled))
     print('Shape of y_train:', np.shape(y_train_oversampled))
     return X_train_oversampled, y_train_oversampled
+
+def plot_roc_curve_alternative(model, X_test, y_test):
+    y_scores = model.predict_proba(X_test)[:, 1]  # Probability estimates for the positive class
+    fpr, tpr, thresholds = roc_curve(y_test, y_scores)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    lw = 2
+    plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = {:.2f})'.format(roc_auc))
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC) Curve')
+    plt.legend(loc='lower right')
+    plt.show()
+
+def evaluate_classification_model(model, X, y, cv=5):
+    # Split the data into training and test sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    # Fit the model on the training data
+    model.fit(X_train, y_train)
+    
+    # Make predictions on the test data
+    y_pred = model.predict(X_test)
+    
+    # Calculate evaluation metrics
+    accuracy = accuracy_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    
+    # Plot the learning curve
+    train_sizes, train_scores, test_scores = learning_curve(model, X_train, y_train, cv=cv)
+    
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    plt.title("Learning Curve")
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+    plt.grid()
+    plt.plot(train_sizes, np.mean(train_scores, axis=1), 'o-', label="Training score")
+    plt.plot(train_sizes, np.mean(test_scores, axis=1), 'o-', label="Cross-validation score")
+    plt.legend(loc="best")
+    
+    # Plot the ROC curve using the alternative method
+    plot_roc_curve_alternative(model, X_test, y_test)
+    
+    plt.tight_layout()
+    plt.show()
+    
+    # Print the evaluation metrics
+    print(f"Accuracy: {accuracy:.2f}")
+    print(f"Recall: {recall:.2f}")
+    print(f"Precision: {precision:.2f}")
+    print(f"F1 Score: {f1:.2f}"
 
 

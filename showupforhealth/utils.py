929fcfb712720ecbe371f9913fb79fca45e0649f
Jan du Plessis <drjanduplessis@icloud.com>
2023-09-07 11:17:52 +0100

    Utils Updated

--- showupforhealth/utils.py
+++ showupforhealth/utils.py
@@ -12,6 +12,7 @@ from sklearn.inspection import permutation_importance
 from tpot import TPOTClassifier
 from datetime import datetime
 
+
 # X = df.drop('Appointment_status', axis=1)
 # y = df['Appointment_status']
 
@@ -23,7 +24,7 @@ def perform_train_test_split(X, y, test_size=0.2, random_state=42):
     
     return X_train, X_test, y_train, y_test
 
-
+# Oversampling with SMOTE
 def oversample_with_smote(X_train, y_train, sampling_strategy='auto', k_neighbors=5, random_state=42):
     smote = SMOTE(sampling_strategy=sampling_strategy, k_neighbors=k_neighbors, random_state=random_state)
     X_train_oversampled, y_train_oversampled = smote.fit_resample(X_train, y_train)
@@ -244,3 +245,4 @@ def define_X_y(df, target):
     print(f'y - dependant variable - {target}: {y.shape}')
 
     return X, y
+

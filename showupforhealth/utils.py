7472acdd8d441b976be48ed4a15d7d1bd0a5da98
Alessio Robotti <alessiorobotti1@gmail.com>
2023-09-07 19:24:29 +0100

    Resolved merge conflit

--- showupforhealth/utils.py
+++ showupforhealth/utils.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-from sklearn.preprocessing import StandardScaler
-
-def standard_scaler(X_train, X_test):
-    scaler = StandardScaler()
-    scaler.fit(X_train)
-    X_train_scaled = scaler.transform(X_train)
-    X_test_scaled = scaler.transform(X_test)
-    return X_train_scaled, X_test_scaled
-=======
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler
 from imblearn.over_sampling import SMOTE
@@ -31,7 +21,7 @@ def perform_train_test_split(X, y, test_size=0.2, random_state=42):
     print(f"✅ OUTPUT: X_train, X_test, y_train, y_test")
     print(f"Train Set:  X_train, y_train - {X_train.shape}, {y_train.shape}")
     print(f" Test Set:  X_test, y_test - - {X_test.shape}, {y_test.shape}")
-    
+
     return X_train, X_test, y_train, y_test
 
 # Oversampling with SMOTE
@@ -39,7 +29,7 @@ def oversample_with_smote(X_train, y_train, sampling_strategy='auto', k_neighbor
     smote = SMOTE(sampling_strategy=sampling_strategy, k_neighbors=k_neighbors, random_state=random_state)
     X_train_oversampled, y_train_oversampled = smote.fit_resample(X_train, y_train)
     print(f'✅ Data Oversampled: SMOTE - X_train_oversampled:{X_train_res.shape} y_train_oversampled:{y_train_res.shape}')
-    
+
     return X_train_oversampled, y_train_oversampled
 
 
@@ -67,7 +57,7 @@ def feature_importance(model, X, y):
     plt.boxplot(result.importances[sorted_idx].T, vert=False, labels=X.columns[sorted_idx])
     plt.title("Permutation Importances")
     plt.show()
-    
+
 def scale_df(df, scaler='minmax'):
     """
     Function to scale the numerical features of a dataframe.
@@ -176,8 +166,8 @@ def evaluate_classification_model(model, X, y, cv=5):
     # Show plots
     plt.tight_layout()
     plt.show()
-    
-    
+
+
 def sample_df(df, n_samples):
     """
     Samples the input DataFrame.
@@ -256,4 +246,9 @@ def define_X_y(df, target):
 
     return X, y
 
->>>>>>> 5dad77d20ef750d1631d13cb0b9f48e190333db4
+def standard_scaler(X_train, X_test):
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train_scaled = scaler.transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+    return X_train_scaled, X_test_scaled
